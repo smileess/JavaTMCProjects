1e9d7e2f054eee76a2a482abca27892333ebe4e8
pozaroszczykk <pozaroszczykk@gmail.com>
2018-10-17 21:56:37 +0200

    Exercise 25 done.

--- mooc-2013-OOProgrammingWithJava-PART1/week2-025.SumOfThreeNumbers/src/SumOfThreeNumbers.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week2-025.SumOfThreeNumbers/src/SumOfThreeNumbers.java
@@ -6,9 +6,19 @@ public class SumOfThreeNumbers {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         int sum = 0;
-        int read; // store numbers read form user in this variable
+        int read;   // store numbers read form user in this variable
 
+        System.out.println("Type the first no.:");
+        read = Integer.parseInt(reader.nextLine());
+        sum = sum+read;
 
+        System.out.println("Type the second no.:");
+        read = Integer.parseInt(reader.nextLine());
+        sum += read;
+
+        System.out.println("Type the third no.:");
+        read = Integer.parseInt(reader.nextLine());
+        sum += read;
         // Write your program here
         // Use only variables sum and read
 
