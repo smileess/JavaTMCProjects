51d5e552904b08abe9463dc1c2bab2ee0c09f902
pozaroszczykk <pozaroszczykk@gmail.com>
2018-10-25 13:48:11 +0200

    034 done

--- mooc-2013-OOProgrammingWithJava-PART1/week2-034.Factorial/src/Factorial.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week2-034.Factorial/src/Factorial.java
@@ -4,5 +4,17 @@ public class Factorial {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
+        System.out.print("Type a number.: ");
+        int n = Integer.parseInt(reader.nextLine());
+
+        int sum = 1; //defining 0! = 1
+        int i = 0;
+
+        while (i < n) {
+
+            i++;
+            sum*= i; // another way of saying sum = sum * i
+        }
+        System.out.println("Factorial is.: "+sum);
     }
 }
