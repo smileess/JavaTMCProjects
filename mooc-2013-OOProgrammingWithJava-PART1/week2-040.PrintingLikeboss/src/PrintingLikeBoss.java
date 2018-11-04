ac07de8c4c7f72e2e743298e47a79f3dcc43b93f
pozaroszczykk <pozaroszczykk@gmail.com>
2018-11-04 13:28:04 +0100

    exercises 35-41 done

--- mooc-2013-OOProgrammingWithJava-PART1/week2-040.PrintingLikeboss/src/PrintingLikeBoss.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week2-040.PrintingLikeboss/src/PrintingLikeBoss.java
@@ -2,27 +2,72 @@ public class PrintingLikeBoss {
 
     // copy or rewrite the method of Assignment 39.1 here
     public static void printStars(int amount) {
+/*  LOOP USAGE
+        for (int i = 0; i < amount; i++) {
+            System.out.print("*");
+
+        }
+        System.out.println();
+    RECURSION USAGE */
+        if (amount == 0) {
+            System.out.println();
+        } else {
+            System.out.print("*");
+            printStars(amount - 1);
+        }
     }
 
     public static void printWhitespaces(int amount) {
         // 40.1
+        if (amount == 0) {
+
+        } else {
+            System.out.print(" ");
+            printWhitespaces(amount - 1);
+        }
     }
 
+    //    public static void printTriangle(int size) {
+//        // 40.2
+//        int spaces = size - 1;
+//        for (int i = 0; i <= size; i++) {
+//
+//            printWhitespaces(spaces--);
+//            printStars(i+1);
+//
+//        }
+//
+//    }
+//  OR WHILE:
     public static void printTriangle(int size) {
         // 40.2
+        int i = 1;
+        int spaces = size - 1;
+
+        while (i <= size) {
+            printWhitespaces(spaces);
+            spaces--;
+            printStars(i);
+            i++;
+        }
     }
 
     public static void xmasTree(int height) {
         // 40.3
+        int spaces = height - 1;
+        for (int i = 0; i < height; i++) {
+            printWhitespaces(spaces--);
+            printStars(2 * i + 1);
+        }
+        printWhitespaces(height - 2);
+        printStars(3);
+        printWhitespaces(height - 2);
+        printStars(3);
     }
 
     public static void main(String[] args) {
         // Tests do not use main, yo can write code here freely!
+        printTriangle(4);
 
-        printTriangle(5);
-        System.out.println("---");
-        xmasTree(4);
-        System.out.println("---");
-        xmasTree(10);
     }
 }
--- mooc-2013-OOProgrammingWithJava-PART1/week2-041.GuessingNumberGame/src/GuessingNumberGame.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week2-041.GuessingNumberGame/src/GuessingNumberGame.java
@@ -7,9 +7,23 @@ public class GuessingNumberGame {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         int numberDrawn = drawNumber();
-
+        int userGuess = 0;
+        int i = 0;
         // program your solution here. Do not touch the above lines!
-        
+
+        while (numberDrawn != userGuess) {
+            userGuess = Integer.parseInt(reader.nextLine());
+            i++;
+            if (userGuess < numberDrawn) {
+                System.out.println("The number is greater, guesses made: "+i);
+            } else if (userGuess > numberDrawn) {
+                System.out.println("The number is lesser, guesses made: "+i);
+            } else if (userGuess == numberDrawn) {
+                System.out.println("Congratulations, your guess is correct!");
+
+            }
+        }
+
     }
 
     // DO NOT MODIFY THIS!
