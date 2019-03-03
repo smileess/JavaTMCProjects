bd1a23f079f88d9cb80b5131ba1114d6366b2273
pozaroszczykk <pozaroszczykk@gmail.com>
2019-03-03 15:42:44 +0100

    exercise 67 finished

--- mooc-2013-OOProgrammingWithJava-PART1/week3-067.TheVariance/src/Variance.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week3-067.TheVariance/src/Variance.java
@@ -1,18 +1,16 @@
 import java.util.ArrayList;
 
-import static java.lang.StrictMath.sqrt;
-
 public class Variance {
     //sum from exercise 63
     public static int sum(ArrayList<Integer> list) {
         int sum = 0;
 
         for (int item : list) {
-            sum+= item;
+            sum += item;
         }
         return sum;
     }
-    
+
     //average from exercise 64
     public static double average(ArrayList<Integer> list) {
         return (double) sum(list) / list.size();
@@ -21,28 +19,30 @@ public class Variance {
     public static double variance(ArrayList<Integer> list) {
 
         // write code here
+        double average = average(list);
         double variance = 0;
         int i = 0;
-        while (i < list.size()) {
-            variance = (sqrt(list.get(i) - average(list)));
+        while (i <= list.size()-1) {
+            System.out.println("execution list pos:" + i);
+
+            variance += Math.pow(list.get(i) - average, 2);
             i++;
 
         }
-        return variance / 4-1;
+        System.out.println("above loop calculation is: " + variance);
+        return variance / (list.size() - 1);
 
 
         // ... / n-1 for Bessel's correction
     }
-    
+
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
         list.add(3);
-        list.add(2);
-        list.add(7);
-        list.add(2);
-        
-        System.out.println("The variance is: " + variance(list));
 
+        System.out.println("The variance is: " + variance(list));
     }
 
 }
